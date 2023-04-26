208d20d70094ab834a8c3921738030b7a74d746a
Arshdeep-13 <108752646+Arshdeep-13@users.noreply.github.com>
2023-04-26 15:40:19 +0530

    Add files via upload

---
+++ Home.dart
@@ -0,0 +1,107 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'main.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  CameraImage? cameraImage;
+  CameraController? cameraController;
+  String output = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadCamera();
+    loadModel();
+  }
+
+  loadCamera() {
+    cameraController = CameraController(cameras![0], ResolutionPreset.medium);
+    cameraController!.initialize().then((value) {
+      if (!mounted) {
+        return;
+      } else {
+        setState(() {
+          cameraController!.startImageStream((imageStream) {
+            cameraImage = imageStream;
+            runModel();
+          });
+        });
+      }
+    });
+  }
+
+  runModel() async {
+    if (cameraImage != null) {
+      var predictions = await Tflite.runModelOnFrame(
+          bytesList: cameraImage!.planes.map((plane) {
+            return plane.bytes;
+          }).toList(),
+          imageHeight: cameraImage!.height,
+          imageWidth: cameraImage!.width,
+          imageMean: 127.5,
+          imageStd: 127.5,
+          rotation: 90,
+          numResults: 2,
+          threshold: 0.1,
+          asynch: true);
+
+      print('Predictions: $predictions');
+
+      predictions!.forEach((element) {
+        print(
+            'Label: ${element['label']}, Confidence: ${element['confidence']}');
+        setState(() {
+          output = element['label'];
+        });
+      });
+    }
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+        model: "assets/model.tflite", labels: "assets/labels.txt");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Emotion Detection App'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.7,
+              width: MediaQuery.of(context).size.width,
+              child: !cameraController!.value.isInitialized
+                  ? Container()
+                  : AspectRatio(
+                      aspectRatio: cameraController!.value.aspectRatio,
+                      child: CameraPreview(cameraController!),
+                    ),
+            ),
+          ),
+          Text(
+            output.isNotEmpty ? output : 'No emotion detected',
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
